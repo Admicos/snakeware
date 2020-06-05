baf8dc847eb16c51fd021022c8338db8ce51648c
Ecmel Berk Canlier <me@ecmelberk.com>
2020-06-05 03:24:40 +0300

    snakeware: utils: Add QEMU script

---
+++ snakeware/qemu.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -eu
+
+RAM="2G"
+ISO="snakeware.iso"
+
+AUDIO="alsa" # change to pa for pulseaudio
+
+exec qemu-system-x86_64 \
+	-drive file="$ISO",media=cdrom,if=virtio \
+	-m "$RAM" \
+	-cpu host \
+	-machine type=q35,accel=kvm \
+	-smp $(nproc) \
+	-audiodev "$AUDIO,id=snd" \
+	-device ich9-intel-hda \
+	-device hda-output,audiodev=snd
